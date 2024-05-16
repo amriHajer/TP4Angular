bea1bb46abbded5275ddd608be00ace5de20d7a5
amriHajer <hajer.amri93@gmail.com>
2024-05-16 16:26:18 +0200

    Premier commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { TaskFormComponent } from './components/task-form/task-form.component';
+import { TasksListComponent } from './components/tasks-list/tasks-list.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TaskFormComponent,
+    TasksListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule ,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
